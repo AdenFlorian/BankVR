0bdcbf6ab18c414cc4799638c36b2cc32304234a
David Valachovic <AdenFlorian@gmail.com>
2016-05-25 23:36:09 -0500

    created a marquee thing

---
+++ Assets/BankVR/Prefabs/Marquee/MarqueeText.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MarqueeText : MonoBehaviour {
+
+    public GameObject thingToScroll;
+
+    [Range(0.0f, 100.0f)]
+    public float scrollSpeed = 1.0f;
+    public float startRightPosX = 1.0f;
+    public float endLeftPosX = -1.0f;
+    
+	void Start() {
+        Vector3 newPos = thingToScroll.transform.position;
+        newPos.x = startRightPosX;
+        thingToScroll.transform.position = newPos;
+	}
+	
+	void Update() {
+        Vector3 newPos = thingToScroll.transform.position;
+        newPos.x -= (scrollSpeed * Time.deltaTime);
+        if (newPos.x < endLeftPosX) {
+            newPos.x = startRightPosX;
+        }
+        thingToScroll.transform.position = newPos;
+    }
+}
