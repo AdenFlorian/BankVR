9eb93a0a466646ad1b90a1201c2723817e4c01ac
AdenFlorian <AdenFlorian@gmail.com>
2016-06-02 08:08:41 -0500

    teleporting

---
+++ Assets/BankVR/Scripts/ViveInput.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ViveInput : MonoBehaviour {
+
+    public static ViveInput Instance;
+
+    public SteamVR_TrackedObject LeftControllerTrackedObj;
+    public SteamVR_TrackedObject RightControllerTrackedObj;
+
+    public bool leftTriggerTouch = false;
+    public bool rightTriggerTouch = false;
+    public bool leftPadTouch = false;
+    public bool rightPadTouch = false;
+
+    // Use this for initialization
+    void Awake () {
+        Instance = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Reset trigger touches
+        leftTriggerTouch = false;
+        rightTriggerTouch = false;
+        leftPadTouch = false;
+        rightPadTouch = false;
+
+
+    SteamVR_Controller.Device deviceLeft = null;
+        SteamVR_Controller.Device deviceRight = null;
+        try {
+            deviceLeft = SteamVR_Controller.Input((int)LeftControllerTrackedObj.index);
+            deviceRight = SteamVR_Controller.Input((int)RightControllerTrackedObj.index);
+        } catch (System.IndexOutOfRangeException) {
+            // Not sure what to do with this yet, only happens on first update
+            //Debug.Log("IndexOutOfRangeException");
+            return;
+        }
+
+
+        //if (device.GetTouch(SteamVR_Controller.ButtonMask.Trigger)) {
+        //    Debug.Log("Holding 'Touch' on Trigger");
+        //}
+
+        if (deviceLeft.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger)) {
+            Debug.Log("Did 'TouchDown' on left Trigger");
+            leftTriggerTouch = true;
+        }
+        if (deviceRight.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("Did 'TouchDown' on right Trigger");
+            rightTriggerTouch = true;
+        }
+        if (deviceLeft.GetTouchDown(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+            Debug.Log("Did 'TouchDown' on left Touchpad");
+            leftPadTouch = true;
+        }
+        if (deviceRight.GetTouchDown(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+            Debug.Log("Did 'TouchDown' on right Touchpad");
+            rightPadTouch = true;
+        }
+        if (deviceLeft.GetPressDown(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+            Debug.Log("Did 'PressDown' on left Touchpad");
+        }
+        if (deviceRight.GetPressDown(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+            Debug.Log("Did 'PressDown' on right Touchpad");
+        }
+    }
+}
---
+++ Assets/BankVR/Scripts/ViveTeleport.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ViveTeleport : MonoBehaviour {
+
+    public float rayLength = 100f;
+    public bool isLeftController = false;
+    public float heightModifier = 0f;
+
+    public Transform cameraRig;
+    public Transform head;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 rayDirection = transform.forward * rayLength;
+
+        Debug.DrawRay(transform.position, rayDirection, Color.red, 0.1f);
+
+        Ray ray = new Ray(transform.position, rayDirection);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, rayDirection, out hit)) {
+            // check if we hit a teleport spot
+            TeleportSpot teleSpot = hit.collider.GetComponent<TeleportSpot>();
+
+            if (teleSpot != null)
+            {
+                if (isLeftController)
+                {
+                    if (ViveInput.Instance.leftTriggerTouch)
+                    {
+                        Teleport(hit.collider.transform.position);
+                    }
+                }
+                else
+                {
+                    if (ViveInput.Instance.rightTriggerTouch)
+                    {
+                        Teleport(hit.collider.transform.position);
+                    }
+                }
+
+                // TODO
+                if (Input.GetMouseButtonDown(0))
+                {
+                    Teleport(hit.collider.transform.position);
+                }
+
+                teleSpot.HighlightOn();
+            }
+
+            
+        }
+
+        
+	}
+
+    void Teleport(Vector3 newPosition)
+    {
+        Vector3 headToRigCenter = cameraRig.position - head.position;
+        headToRigCenter.y = 0f;
+
+        newPosition.y = newPosition.y + heightModifier;
+        cameraRig.position = newPosition + headToRigCenter;
+    }
+}
